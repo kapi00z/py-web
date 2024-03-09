2fef6fb344042bc6884afe44d335e26b72c1bf0e
kapi00z <hnerno.o@gmail.com>
2024-03-09 07:30:34 +0000

    auto commit

--- web/main.py
+++ web/main.py
@@ -1,17 +1,10 @@
-from flask import Flask, jsonify
-import books, json
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello!"
-
-@app.route('/books')
-def bookList():
-    with open(f"{books.bookDir}/books.json", 'r') as file:
-        data = json.loads(file.read())
-    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
